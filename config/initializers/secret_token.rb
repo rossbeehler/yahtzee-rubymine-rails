48a26b98fb29bb06e8625c04978e8777c2f7968a
Ross Beehler <ross@sipp.net>
2011-11-04 23:43:51 -0400

    Rubymine generated rails app pointing to sqlite3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yahtzee::Application.config.secret_token = '65d269fb6533114c1a1cb3e7083c1e9caf322f791591f5ff5a9e2d868dae74ab1a9ae74286ca87527ae7a04370f20a1313fcb05e01c91a1b2dfe2709fe6bb086'
